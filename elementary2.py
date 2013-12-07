9c6cdca0714d1ad20112216c2c0f900f181aa52f
Lauri Hakko <lauri.hakko@gmail.com>
2013-12-07 22:27:25 +0200

    implemented elementary 2

---
+++ elementary2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#
+# Write a program that asks the user for his name and greets him with his name.
+
+print("What is your name? ", end="")
+name = input()
+print("Hello, ", name)
+
+
